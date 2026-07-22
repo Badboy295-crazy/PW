5b739bbdb6ff7ca3effdf85099ed25c5916b0417
Badboy295-crazy <amberkasaudhan37@gmail.com>
2026-07-22 20:20:30 +0530

    Test super=true in test-scrape diagnostic endpoint

--- api/test-scrape.js
+++ api/test-scrape.js
@@ -1,7 +1,7 @@
 module.exports = async function handler(req, res) {
   const token = 'ba70ac83bc71441481a4f6f1f6d469b26be12eca8fd';
   const targetUrl = encodeURIComponent('https://deltastudy.site/verify');
-  const scrapeDoUrl = `https://api.scrape.do?token=${token}&url=${targetUrl}&pureCookies=true&render=true&customWait=5000`;
+  const scrapeDoUrl = `https://api.scrape.do?token=${token}&url=${targetUrl}&pureCookies=true&render=true&customWait=5000&super=true`;
 
   try {
     const t0 = Date.now();
